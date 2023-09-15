fc7ea68081a4b792e3ba59c9eb2b58c3cc8cabd0
Carlosnoe10 <carlosdnoem@gmail.com>
2023-09-15 13:12:47 -0700

    Gui Terminado y clase hecha

--- src/lab9progra2_carlosnoé/Dba.java
+++ src/lab9progra2_carlosnoé/Dba.java
@@ -1,14 +1,47 @@
 package lab9progra2_carlosnoé;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-/**
- *
- * @author cd507
- */
 public class Dba {
     
+
+    private String base_datos;
+    private Connection dbcon;
+    public Statement query;
+
+    public Dba(String base_datos) {
+        this.base_datos = base_datos;
+    }
+
+    public void setBase_datos(String base_datos) {
+        this.base_datos = base_datos;
+    }
+
+    public void conectar() {
+        try {
+            String driver = "jdbc:ucanaccess://";
+            dbcon = DriverManager.getConnection(driver + base_datos, "", "");
+            query = dbcon.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void desconectar() {
+        try {
+            query.close();
+            dbcon.close();
+        } catch (Exception e) {
+        }
+    }
+
+    public void commit() {
+        try {
+            dbcon.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
